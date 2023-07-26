adefcaf23dfa5fec864e59dc4bbb6e4dda8fcaca
Yunyi Liang <liang@uni-hildesheim.de>
2023-07-26 19:41:21 +0200

    new generate .sh

--- modify_script.py
+++ modify_script.py
@@ -1,34 +1,55 @@
-script_file = "~/projects/thesis-repo/sbatch_test.sh"
+import argparse
 
-# Open the existing shell script in read mode
-with open(script_file, 'r') as file:
-    script_content = file.read()
+def create_new_sh_content(args):
+    new_sh_content = f'''#!/usr/bin/env bash
+    #SBATCH --job-name=experiment
+    #SBATCH --output=experiment%j.log
+    #SBATCH --error=experiment%j.err
+    #SBATCH --mail-user=liang@uni-hildesheim.de
+    #SBATCH --mail-type=ALL
+    #SBATCH --partition=STUD
+    #SBATCH --gres=gpu:1
 
-# Replace the desired parameters with new values
-new_tensorboard_comment = "test food101 vaeAug DEBUG"
-# new_augmentation_type = "new_augmentation_type_value"
-# new_simpleAugmentation_name = "new_simpleAugmentation_name_value"
+    source activate toyEnv
+    srun python experiment01.py \\
+    --dataset {args.dataset} \\
+    --entropy_threshold {args.entropy_threshold} \\
+    --run_epochs {args.run_epochs} \\
+    --candidate_start_epoch {args.candidate_start_epoch} \\
+    --tensorboard_comment "{args.tensorboard_comment}" \\
+    --lr {args.lr} \\
+    --l2 {args.l2} \\
+    --batch_size {args.batch_size} \\
+    {"--not_pretrained" if args.not_pretrained else ""} \\
+    {"--reduce_dataset" if args.reduce_dataset else ""} \\
+    {"--augmentation_flag" if args.augmentation_flag else ""} \\
+    {"--augmentation_type " + args.augmentation_type if args.augmentation_type else ""} \\
+    {"--simpleAugmentaion_name " + args.simpleAugmentaion_name if args.simpleAugmentaion_name else ""}
+'''
 
-# Modify the script content with the new parameter values
-script_content = script_content.replace(
-    '--tensorboard_comment "food101 vaeAug DEBUG"',
-    f'--tensorboard_comment "{new_tensorboard_comment}"'
-)
+    return new_sh_content
 
-# script_content = script_content.replace(
-#     '--augmentation_type vae',
-#     f'--augmentation_type {new_augmentation_type}'
-# )
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='create new sh file for experiment')
 
-# # Note: Modify this line according to your actual script content
-# # This is just an example; you need to find the exact string to replace
-# script_content = script_content.replace(
-#     'your_existing_simpleAugmentation_name_value',
-#     new_simpleAugmentation_name
-# )
+    parser.add_argument('--dataset', type=str, default='MNIST', choices=("MNIST", "CIFAR10", "FashionMNIST", "SVHN", "Flowers102", "Food101"), help='Dataset name')
+    parser.add_argument('--entropy_threshold', type=float, default=0.5, help='Entropy threshold')
+    parser.add_argument('--run_epochs', type=int, default=5, help='Number of epochs to run')
+    parser.add_argument('--candidate_start_epoch', type=int, default=0, help='Epoch to start selecting candidates. Candidate calculation begind after the mentioned epoch')
+    parser.add_argument('--tensorboard_comment', type=str, default='test_run', help='Comment to append to tensorboard logs')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--l2', type=float, default=1e-4, help='L2 regularization')
+    parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training (default: 64)')
+    parser.add_argument('--not_pretrained', action='store_true', help='Use randomly initialized weights instead of pretrained weights')
+    parser.add_argument('--reduce_dataset', action='store_true', help='Reduce the dataset size (for testing purposes only)')
+    parser.add_argument('--augmentation_flag', action='store_true', help='Not augmenting the candidate samples')
+    parser.add_argument('--augmentation_type', type=str, default=None, choices=("vae", "simple"), help='Augmentation type')
+    parser.add_argument('--simpleAugmentaion_name', type=str, default=None, choices=("random_color", "center_crop", "gaussian_blur", "elastic_transform", "random_perspective", "random_resized_crop", "random_invert", "random_posterize", "rand_augment", "augmix"), help='Simple Augmentation name')
 
-# Open the script in write mode and overwrite the content with the updated version
-with open(script_file, 'w') as file:
-    file.write(script_content)
+    args = parser.parse_args()
 
-print(f"Updated the content of '{script_file}'")
+    new_sh_content = create_new_sh_content(args)
+    with open('new_testScript.sh', 'w') as file:
+        file.write(new_sh_content)
+    print(f"Updated the content of 'new_testScript.sh'")
+
