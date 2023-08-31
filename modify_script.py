ab282b807290f9174d541a5c667daf460e084d36
Yunyi Liang <liang@uni-hildesheim.de>
2023-08-31 17:48:57 +0200

    debug+2 modify_script.sh

--- modify_script.py
+++ modify_script.py
@@ -24,9 +24,10 @@ srun python exp.py \\
     --batch_size {args.batch_size} \\
     --vae_accumulationSteps {args.vae_accumulationSteps} \\
     --accumulation_steps {args.accumulation_steps} \\
-    {"--not_pretrained" if args.not_pretrained else ""} \\
     {"--reduce_dataset" if args.reduce_dataset else ""} \\
     {"--augmentation_type " + args.augmentation_type if args.augmentation_type else ""} \\
+    {"--k_epoch_sampleSelection " + args.k_epoch_sampleSelection if args.k_epoch_sampleSelection else ""} \\
+    {"--augmente_epochs_list" + args.augmente_epochs_list if args.augmente_epochs_list else ""}
     {"--simpleAugmentaion_name " + args.simpleAugmentaion_name if args.simpleAugmentaion_name else ""}
 '''
     return new_sh_content
@@ -34,7 +35,7 @@ srun python exp.py \\
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='create new sh file for experiment')
 
-    parser.add_argument('--dataset', type=str, default='MNIST', choices=("MNIST", "CIFAR10", "FashionMNIST", "SVHN", "Flowers102", "Food101"), help='Dataset name')
+    parser.add_argument('--dataset', type=str, default='CIFAR10', choices=("MNIST", "CIFAR10", "FashionMNIST", "SVHN", "Flowers102", "Food101"), help='Dataset name')
     parser.add_argument('--entropy_threshold', type=float, default=0.5, help='Entropy threshold')
     parser.add_argument('--run_epochs', type=int, default=5, help='Number of epochs to run')
     parser.add_argument('--candidate_start_epoch', type=int, default=0, help='Epoch to start selecting candidates. Candidate calculation begind after the mentioned epoch')
@@ -42,12 +43,15 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
     parser.add_argument('--l2', type=float, default=1e-4, help='L2 regularization')
     parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training (default: 64)')
-    parser.add_argument('--not_pretrained', action='store_true', help='Use randomly initialized weights instead of pretrained weights')
     parser.add_argument('--reduce_dataset', action='store_true', help='Reduce the dataset size (for testing purposes only)')
     parser.add_argument('--augmentation_type', type=str, default=None, choices=("vae", "simple"), help='Augmentation type')
-    parser.add_argument('--simpleAugmentaion_name', type=str, default=None, choices=("random_color", "center_crop", "gaussian_blur", "elastic_transform", "random_perspective", "random_resized_crop", "random_invert", "random_posterize", "rand_augment", "augmix"), help='Simple Augmentation name')
-    parser.add_argument('--accumulation_steps', type=int, default=2, help='Number of accumulation steps')
-    parser.add_argument('--vae_accumulationSteps', type=int, default=3, help='Accumulation steps for VAE training')
+    parser.add_argument('--simpleAugmentaion_name', type=str, default=None, choices=("random_color", "center_crop", "gaussian_blur", 
+                                                                                    "elastic_transform", "random_perspective", "random_resized_crop", 
+                                                                                    "random_invert", "random_posterize", "rand_augment", "augmix"), help='Simple Augmentation name')
+    parser.add_argument('--accumulation_steps', type=int, default=None, help='Number of accumulation steps')
+    parser.add_argument('--vae_accumulationSteps', type=int, default=4, help='Accumulation steps for VAE training')
+    parser.add_argument('--k_epoch_sampleSelection', type=int, default=None, help='Number of epochs to select the common candidates')
+    parser.add_argument('--augmente_epochs_list', type=list, default=None, help='Number of epochs to train VAE')
     args = parser.parse_args()
 
     new_sh_content = create_new_sh_content(args)
