38e34f146180873c4eca6ff77a97d8d1d2b8c826
Yunyi Liang <liang@uni-hildesheim.de>
2024-01-10 23:21:54 +0100

    change kernel_size and maxpook

--- exp.py
+++ exp.py
@@ -95,13 +95,15 @@ if __name__ == '__main__':
     # for param in resnet.parameters():
     #   param.requires_grad = False
     resnet.fc = nn.Linear(resnet.fc.in_features, classes_num)
-    if args.dataset == 'CIFAR10':
-      resnet.maxpool = torch.nn.Identity()
+    resnet.maxpool = torch.nn.Identity()
   else:
     print('using non-pretrained resnet')
     resnet = resnet18(weights=None, num_classes=classes_num)
 
-  resnet.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+  if args.dataset in ['MNIST', 'CIFAR10']:
+    resnet.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+  elif args.dataset in ['CINIC10']:
+    resnet.conv1 = torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, stride=1, padding=1, bias=False)
   
   resnet_boardComment, vae_boardComment = boardWriter_generator(args)
   print('RESNET board comment: ', resnet_boardComment)
